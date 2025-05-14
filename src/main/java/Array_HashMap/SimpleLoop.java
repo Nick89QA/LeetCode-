45c6a8fd55895d3632182256eb354c53676a8aa4
Nickolay Petrych <npetrych@alfabank.ru>
2025-05-14 18:49:36 +0100

    Loop day N3

--- src/main/java/Array_HashMap/SimpleLoop.java
+++ src/main/java/Array_HashMap/SimpleLoop.java
@@ -3,11 +3,11 @@ package Array_HashMap;
 public class SimpleLoop {
     public static void main(String[] args) {
         /**
-         * сумма двух чисел
+         * вывод элементов масива вместе с индексом
          */
 
         int nums1[] = {1, 2, 4, 5}; //declaring an array
-        int target = 3; //declaring integer
+
 
         // loop through array
         for (int i = 0; i < nums1.length; i++) {
@@ -31,14 +31,40 @@ public class SimpleLoop {
          */
         // check if the number is even
         // if the number is separable
-        int nums2[] = {1,2,3,4,5,6,7,8};
-        for (int i=0; i< nums2.length;i++) {
-            if(nums2[i] % 2 == 0){
+        int nums2[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        for (int i = 0; i < nums2.length; i++) {
+            if (nums2[i] % 2 == 0) {
                 System.out.println(" This is an even number " + nums2[i]);
-            }else {
+            } else {
                 System.out.println("This is odd number " + nums2[i]);
             }
         }
+      /**
+       * перебрать все уникальные пары чисел в массиве
+       */
+
+        int[] sum4 = {1, 2, 4, 5, 5, 6, 4, 5};
+        for (int i = 0; i < sum4.length; i++) {
+            for (int j = i + 1; j < sum4.length; j++) {
+                System.out.println(" Pair " + sum4[i] + " and " + sum4[j]);
+                // if (sum4[i] + sum4[j] == target4){
+                //  System.out.println( i + " " + j );
+                //}
+
+            }
+        }
+
+        int [] sum5 = {1,2,33,333,443,45};
+        int target5 = 3;
+        for (int i = 0; i<sum5.length; i++){
+            for (int j = i + 1; j<sum5.length;j++) {
+                if (sum5[i] + sum5[j] == target5){
+                    System.out.println(" Indices " + i + " and " + j);
+                }
+            }
+        }
+
+
     }
 }
 
