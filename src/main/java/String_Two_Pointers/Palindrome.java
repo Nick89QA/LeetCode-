d4d4ff0927b856d4ded7a86ac10d458dcd8b11e0
Nickolay Petrych <npetrych@alfabank.ru>
2025-05-16 21:21:08 +0100

    Palindrome day №1

---
+++ src/main/java/String_Two_Pointers/Palindrome.java
@@ -0,0 +1,12 @@
+package String_Two_Pointers;
+
+public class Palindrome {
+    public static void main(String[] args) {
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;//обход строки с двух сторон
+    }
+
+}
