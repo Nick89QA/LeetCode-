d1b730c7fdcd0c137fd8e4bb3cba6d1b25e00221
Nickolay Petrych <npetrych@alfabank.ru>
2025-05-19 18:23:42 +0100

    Palindrome day №4

---
+++ src/main/java/Array_HashMap/trainingIfElse.java
@@ -0,0 +1,23 @@
+package Array_HashMap;
+
+public class trainingIfElse {
+    public static void main(String[] args) {
+        // считаем сколько цифр в строке
+        char f = 5;
+        int count = 0;
+        if (Character.isDigit(f)) {
+            count++;
+            System.out.println("Это цифра");
+        } else {
+            System.out.println("Нет");
+        }
+
+        String s = "abc123d4";
+
+        for (int i = 0; i <s.length();i++){
+
+        }
+
+    }
+}
+
